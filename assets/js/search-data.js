827ef5d292a77825a5d71d79af196abae9b375ca
amirhsarkaboudi <121556174+amirhsarkaboudi@users.noreply.github.com>
2025-09-24 21:08:21 +0000

    Deploying to gh-pages from @ amirhsarkaboudi/amirhsarkaboudi.github.io@e1bcc41e711d67ded512f1a06a862963ccf9e270 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "You can download my last version(9/24/2025) cv by clicking the PDF icon.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
